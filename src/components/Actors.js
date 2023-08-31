3451f262b61f7afd4b70620b106cb1db069a3a33
leon <leon.waga@student.moringaschool.comgit config --global user.name leongit config --global user.name leon>
2023-08-31 13:35:43 +0300

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const act = actors.map((actor, index) => {
+    return <div key={actor+index}>
+    <h2>Name: {actor.name}</h2>
+    <h5>Movies</h5>
+  <ul>
+    {actor.movies.map((list, index) =>{
+      return<li key={list+index}>{list}</li>
+    })}
+  </ul>
+  </div>
+})
+
+return(
+      <div>
+        <h1>Actors Page</h1>
+        {Act}
+      </div>
+  )
 }
 
 export default Actors;
