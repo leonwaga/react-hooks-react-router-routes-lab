9f2aa6910d8d6d5e3b1fe888a69de83e869ee63d
leon <leon.waga@student.moringaschool.comgit config --global user.name leongit config --global user.name leon>
2023-08-31 12:31:05 +0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,24 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar/>
+      <switch>
+       <Route exact path="/movies">
+        <Movies/>
+        </Route>
+        <Route exact path="/directors">
+        </Route>
+        <Route exact path="/actors">
+        <Actors/>
+        </Route>
+        <Route exact path="/">
+        <Home/>
+      </Route>
+    </switch>
+  </div>
+  )
 }
 
 export default App;
